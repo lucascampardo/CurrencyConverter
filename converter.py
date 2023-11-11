d0d57d79ff2c1fc699c3e207580d7db333cf1103
lucas <lucascampardo@outlook.com>
2023-11-10 23:09:17 -0300

    I did some upgrades on the code structure

--- converter.py
+++ converter.py
@@ -1,27 +1,30 @@
-global dollar
-global euro
+def converter(amount, currency_rate):
+    try:
+        amount = float(amount)
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return 
     
-def converter():
-    global dollar
-    dollar = 4.80
-    global euro
-    euro = 5.60
+    converted_amount = amount / currency_rate
+    return converted_amount
+
+def main():
+    exchange_rates = {'D': 4.80, 'E': 5.60}
     
     print("D - Dollar  |  E - Euro")
-    op = str(input('You want to convert to Dollar or Euro? '))
+    opt = input("You want to convert to Dollar or Euro? ").upper()
     
-    if op == 'D':
-        real = float(input("Type the number in Brazilian Real: "))
-        converter = real / dollar
-        print("Alright, you have {:.2f} dollars now!".format(converter))
-    elif op == 'E':
-        real = float(input("Type the number in Brazilian Real: "))
-        converter = real / euro
-        print("Good, you have {:.2f} euros now!".format(converter))
-    else:
+    if opt in exchange_rates:
+        try:
+            amount = float(input("Type the number in Brazilian Real: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            return 
+        
+        converted_amount = converter(amount, exchange_rates[opt])
+        print("You have {:.2f} now!".format(converted_amount, opt))
+    else: 
         print("Invalid Option!")
-
-converter()
-    
-    
-    
+        
+if __name__ == "__main__":
+    main()
